ec316a4b80d107b4027b81297c2391a29116c920
Junior <mjmabunda0@gmail.com>
2024-01-25 09:47:36 +0200

    Switch to regular nested for loop

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_matrix = [[row[i]**2 for row in matrix] for i in range(len(matrix))]
+    square_matrix = []
+    for row in matrix:
+        square_row = []
+        for col in row:
+            square_row.append(col**2)
+
+        square_matrix.append(square_row)
+
     return square_matrix
