a5a6fa43ce5eb4674db9167ed3455bbbc463d705
Junior <mjmabunda0@gmail.com>
2024-01-25 10:06:20 +0200

    Handle I when it appears before V

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
     # handle bad input
     if roman_string is None or type(roman_string) is not str:
         return 0
+    if len(roman_string) == 0:
+        return 0
 
-    # reference to a few common/reused numerals
+    # reference to a few common/reused numerals, up to 1000
     table = {
         "I": 1,
         "V": 5,
@@ -15,24 +17,34 @@ def roman_to_int(roman_string):
         "M": 1000
     }
 
-    answer = 0  # will store final resuls
+    answer = 0  # will store the final result
     skip = 0  # number of times I is skipped
     previous = ""  # reference to the previous letter
 
     for letter in roman_string:
+        # count I's as a group
         if letter == "I":
             skip += 1
-        # when X is encountered, subtract the number of I's
-        # that appear before X
+        # when X or V is encountered, subtract the number of I's
+        # that appeared before X or V
         elif letter == "X":
             if previous == "I":
                 answer = answer + table[letter] - skip
             else:
                 answer = answer + table[letter]
             skip = 0
+
+        elif letter == "V":
+            if previous == "I":
+                answer = answer + table[letter] - skip
+            else:
+                answer = answer + table[letter]
+            skip = 0
+
         else:
             answer = answer + table[letter] + skip
             skip = 0
+
         previous = letter
 
     # add trailing I's
