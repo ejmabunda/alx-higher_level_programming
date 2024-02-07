84403367ad02966a0222c957183585fb118ea875
Junior <mjmabunda0@gmail.com>
2024-02-07 10:19:21 +0200

    Python scripts should start with #!/usr/bin/python3

--- 0x05-python-exeptions/100-safe_print_integer_err.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-    except ValueError as e:
-        print(f"Exception: {e}", file=sys.stderr)
--- 0x05-python-exeptions/5-raise_exception.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def raise_exception():
-    try:
-        raise Exception
-    except Exception:
-        print("Exception raised")
