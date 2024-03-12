60d3a1149158101016a39f776213e39d28f2ee7e
Junior Mabunda <mjmabunda0@gmail.com>
2024-03-12 14:55:59 +0200

    [UPDATE] Add id in Square constructor

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Represents the Square geometric shape"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Returns a string representation of a Square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
