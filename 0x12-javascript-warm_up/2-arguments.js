772fdf8a388bb82b2dee981254834aa5c32c7b55
Junior Mabunda <mjmabunda0@gmail.com>
2024-04-22 12:14:42 +0200

    Remove script from argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length === 2) {
-  console.log('No argument');
-} else if (argv.length === 3) {
-  console.log('Argument found');
+// Exclude script name
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+  console.log("No argument");
+} else if (argv.length === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
