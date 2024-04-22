b9d46ec371ea60db40540c80b7ba7f136912fde4
Junior Mabunda <mjmabunda0@gmail.com>
2024-04-22 16:13:30 +0200

    Handle negative input and no input

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
-const args = process.argv;
+const args = process.argv.slice(2).map(Number);
 
 console.log(secondBiggest(args));
 
 function secondBiggest (args) {
-  let biggest = 0
-  let second_biggest = 0;
-  
-  for (let a = 2; a < args.length; a++) {
+  if (args.length <= 1) {
+    return 0;
+  }
+
+  let biggest = -Infinity;
+  let second_biggest = -Infinity;
+
+  for (let a = 0; a < args.length; a++) {
     if (args[a] > biggest) {
       second_biggest = biggest;
       biggest = args[a];
+    } else if (args[a] > second_biggest && args[a] < biggest) {
+      second_biggest = args[a];
     }
   }
   return second_biggest;
