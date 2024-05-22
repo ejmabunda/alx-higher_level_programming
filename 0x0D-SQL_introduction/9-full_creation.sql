0ee6bb047641b4bac3acc5bba06b54cf922f7b27
Junior Mabunda <mjmabunda0@gmail.com>
2024-05-22 14:46:17 +0200

    Add script that creates table and adds rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Creates a table second_table in the database hbtn_0c_0
+-- and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) DEFAULT NULL,
+  score INT DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+
+INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
